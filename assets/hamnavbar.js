dd1f9c2fc134af01bd31d5486230e98254d7da75
Eleven <11@apptime.tech>
2021-08-04 01:06:55 +0300

    added

---
+++ assets/hamnavbar.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("YanMenu").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("YanMenu").style.width = "0";
+  }
+  
+  function back(){
+    history.back()
+  }
